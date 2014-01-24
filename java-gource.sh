c557b147acdc0c8cba5b84af888b3246d20e0b1a
Dmitry Sidorenko <dsidorenko@agent.ru>
2014-01-24 11:28:46 +0400

    Initial

---
+++ java-gource.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while read -r commit_line; do
+	sh1=$(echo "$commit_line" | cut -d ';' -f1 -)
+	timestamp=$(echo "$commit_line" | cut -d ';' -f2 -)
+	author=$(echo "$commit_line" | cut -d ';' -f3 -)
+	# echo $timestamp $author
+	while read -r file_line; do
+		file=$(echo $file_line | cut -c3- | sed -e 's/.*\/src\/main\/java\///' -e 's/.*\/src\/test\/java\///')
+
+		changes=$(echo $file_line | cut -c1)
+	    echo "$timestamp|$author|$changes|$file"
+	done < <(git diff-tree --name-status -r --no-commit-id  $sh1 | grep '.java$')
+done < <(git log --pretty="%H;%ct;%cN" --no-merges --diff-filter=AMD --reverse $1..$2)
