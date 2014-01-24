1d143a0e66bbfa08992306047f601d1f67d603ca
Dmitry Sidorenko <dsidorenko@agent.ru>
2014-01-24 11:56:15 +0400

    using author, not commiter

--- java-gource.sh
+++ java-gource.sh
@@ -14,8 +14,7 @@ while read -r commit_line; do
 	author=$(echo "$commit_line" | cut -d ';' -f3 -)
 	while read -r file_line; do
 		file=$(echo $file_line | cut -c3- | sed -e 's/.*\/src\/main\/java\///' -e 's/.*\/src\/test\/java\///')
-
 		changes=$(echo $file_line | cut -c1)
 	    echo "$timestamp|$author|$changes|$file"
 	done < <(git diff-tree --name-status -r --no-commit-id  $sh1 | grep '.java$')
-done < <(git log --pretty="%H;%ct;%cN" --no-merges --diff-filter=AMD --reverse $1..$2)
+done < <(git log --pretty="%H;%at;%aN" --no-merges --diff-filter=AMD --reverse $1..$2)
